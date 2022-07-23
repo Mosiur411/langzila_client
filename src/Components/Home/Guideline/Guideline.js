17a7d0b633ca16c249d4306491ab7f1cdd67968e
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-07-23 22:51:32 +0600

    Guideline Section added

---
+++ src/Components/Home/Guideline/Guideline.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import kid from '../../assets/GuidelineImage/kid.png'
+import { BsCheckCircleFill, BsFillBookmarkPlusFill } from "react-icons/bs";
+import { FaSignal } from "react-icons/fa";
+import { AiFillSmile } from "react-icons/ai";
+
+const Guideline = () => {
+    return (
+        <div className='bg-[#EEE8DF] font-nunito pb-10'>
+            <div className='text-center'>
+                <h3 className='text-4xl font-bold text-[#221638]'>Join With us</h3>
+                <h2 className='text-2xl text-[#FE4A55]'>Guideline</h2>
+            </div>
+            <div className='md:flex lg:flex justify-center items-center'>
+                <div>
+                    <img src={kid} alt="" />
+                </div>
+                <div>
+                    <h2 className='font-black text-3xl text-[#221638] py-4'>Find Out Why You Should <br /> Learn With Verbalizer</h2>
+                    <div className='grid grid-cols-2 gap-4'>
+                        <div className='bg-white w-80 p-4'>
+                            <div className='flex items-center'>
+                                < BsCheckCircleFill className='text-[#FE4A55] mr-2' />
+                                <h3 className='font-bold text-[#221638]'>Create Account</h3>
+                            </div>
+                            <h4>Lorem ipsum dolor sit amet consecte nadipiscing elit</h4>
+                        </div>
+                        <div className='bg-white w-80 p-4'>
+                            <div className='flex items-center'>
+                                < BsFillBookmarkPlusFill className='text-[#FE4A55] mr-2' />
+                                <h3 className='font-bold text-[#221638]'>Select Course</h3>
+                            </div>
+                            <h4>Lorem ipsum dolor sit amet consecte nadipiscing elit</h4>
+                        </div>
+                        <div className='bg-white w-80 p-4'>
+                            <div className='flex items-center'>
+                                < FaSignal className='text-[#FE4A55] mr-2' />
+                                <h3 className='font-bold text-[#221638]'>Learn Your Skill</h3>
+                            </div>
+                            <h4>Lorem ipsum dolor sit amet consecte nadipiscing elit</h4>
+                        </div>
+                        <div className='bg-white w-80 p-4'>
+                            <div className='flex items-center'>
+                                < AiFillSmile className='text-[#FE4A55] mr-2' />
+                                <h3 className='font-bold text-[#221638]'>Success Story</h3>
+                            </div>
+                            <h4>Lorem ipsum dolor sit amet consecte nadipiscing elit</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default Guideline;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import AboutUs from './AboutUs/AboutUs';
 import Feedback from './Feedback/Feedback';
 import Footer from './Footer/Footer';
+import Guideline from './Guideline/Guideline';
 import LatestBlog from './LatestBlog/LatestBlog';
 import Subscribe from './Subscribe/Subscribe';
 
 const Home = () => {
     return (
         <div>
+            <Guideline></Guideline>
             <Feedback></Feedback>
             <AboutUs></AboutUs>
             <div className='mb-24'>
