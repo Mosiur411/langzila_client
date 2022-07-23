3de5a9832d382dfa1c85c1f9c90aaca0e141ad2f
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-07-23 21:07:08 +0600

    navbar samthing edit

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            This is a navbar.
+            This is a Fast git problem solve insoallha 
         </div>
     );
 };
