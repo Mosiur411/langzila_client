bd2b7c9aabf3db647b9c361f0f1dd90567a37b0e
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-07-24 07:58:55 +0600

    Anumation cursor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import DevSixer from "./Components/DevSixer";
+import AnimatedCursor from "react-animated-cursor"
+
 function App() {
   return (
     <div>
       <DevSixer></DevSixer>
+      <AnimatedCursor
+        innerSize={15}
+        outerSize={8}
+        color='255, 255, 0'
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
+      />
     </div>
   );
 }
