e8a4b8cbce1d5e004a28d94118c1f5ec1297b9e1
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-07-24 20:22:54 +0600

    Border Radious added to Guideline section card

--- src/Components/Home/Guideline/Guideline.js
+++ src/Components/Home/Guideline/Guideline.js
@@ -18,28 +18,28 @@ const Guideline = () => {
                 <div>
                     <h2 className='font-black text-3xl text-[#221638] py-4'>Find Out Why You Should <br /> Learn With Verbalizer</h2>
                     <div className='grid grid-cols-2 gap-4'>
-                        <div className='bg-white w-80 p-4'>
+                        <div className='bg-white md:w-80 lg:w:80 p-4 rounded-md'>
                             <div className='flex items-center'>
                                 < BsCheckCircleFill className='text-[#FE4A55] mr-2' />
                                 <h3 className='font-bold text-[#221638]'>Create Account</h3>
                             </div>
                             <h4>Lorem ipsum dolor sit amet consecte nadipiscing elit</h4>
                         </div>
-                        <div className='bg-white w-80 p-4'>
+                        <div className='bg-white md:w-80 lg:w:80 p-4 rounded-md'>
                             <div className='flex items-center'>
                                 < BsFillBookmarkPlusFill className='text-[#FE4A55] mr-2' />
                                 <h3 className='font-bold text-[#221638]'>Select Course</h3>
                             </div>
                             <h4>Lorem ipsum dolor sit amet consecte nadipiscing elit</h4>
                         </div>
-                        <div className='bg-white w-80 p-4'>
+                        <div className='bg-white md:w-80 lg:w:80 p-4 rounded-md'>
                             <div className='flex items-center'>
                                 < FaSignal className='text-[#FE4A55] mr-2' />
                                 <h3 className='font-bold text-[#221638]'>Learn Your Skill</h3>
                             </div>
                             <h4>Lorem ipsum dolor sit amet consecte nadipiscing elit</h4>
                         </div>
-                        <div className='bg-white w-80 p-4'>
+                        <div className='bg-white md:w-80 lg:w:80 p-4 rounded-md'>
                             <div className='flex items-center'>
                                 < AiFillSmile className='text-[#FE4A55] mr-2' />
                                 <h3 className='font-bold text-[#221638]'>Success Story</h3>
