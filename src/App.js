5557e8a15a633b0f1190f071445540088f10735b
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-07-25 22:51:54 +0600

    hover prolem

--- src/App.js
+++ src/App.js
@@ -30,24 +30,15 @@ function App() {
 
       
       <AnimatedCursor
-        innerSize={15}
-        outerSize={8}
         color='254, 74, 85'
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={5}
+     
         clickables={[
-          'a',
           'input[type="text"]',
           'input[type="email"]',
           'input[type="number"]',
           'input[type="submit"]',
           'input[type="image"]',
           'label[for]',
-          'select',
-          'textarea',
-          'button',
-          '.link'
         ]}
       />
     </div>
