69bdc55e5b419b792b1cb98d2768d465a52ad299
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-07-26 16:28:37 +0600

    sound add

--- src/Components/CapitalGames/CapitalGames.js
+++ src/Components/CapitalGames/CapitalGames.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import A from '../assets/Audio/funnysound.wav'
 const CapitalGames = () => {
+    const [CapitalLetter, setCapitalLetter] = useState([[]])
+    useEffect(() => {
+        fetch('English.json')
+            .then(res => res.json())
+            .then(data => {
+                setCapitalLetter(data)
+            })
+    })
+
+
+
+
+
     const AudioPlay = (latter) => {
-
-
         /* ============== swithc =================  */
         switch (latter) {
-            case 'a':
+            case latter:
                 var audio = new Audio(A);
                 audio.play();
                 break;
@@ -16,12 +27,18 @@ const CapitalGames = () => {
     }
     return (
         <div className='px-2 md:px-10 font-bold uppercase'>
-            <h1 className="text-3xl text-center py-10">CapitalGames</h1>
-            <div className=''>
-                <div
-                    onMouseEnter={() => AudioPlay('a')}
-                    className='w-[150px] h-[150px] text-2xl flex justify-center items-center bg-gray-800 text-white rounded'
-                >A</div>
+            <h1 className="text-4xl text-center py-10">CapitalGames</h1>
+            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-10'>
+                {
+                    CapitalLetter.map(word =>
+                        <div
+                            onMouseEnter={() => AudioPlay(word.Latter)}
+                            className='w-[150px] h-[150px] mx-auto text-2xl flex justify-center items-center bg-[#FE4A55]
+                            hover:shadow-amber-500 hover:shadow-2xl text-white rounded'
+                        >{word.Latter}</div>
+                    )
+                }
+
             </div>
         </div>
     );
