8c7935dae5fd961ba9626d5653f021cc846ce455
arfat132 <arfatbegum90@gmail.com>
2022-07-26 17:38:22 +0300

    authentication implemented

--- src/Components/DevSixer.js
+++ src/Components/DevSixer.js
@@ -1,12 +1,15 @@
 import {Routes,Route } from 'react-router-dom';
+import SignIn from './Auth/SignIn';
+import SignUp from './Auth/SignUp';
 import Home from './Home/Home';
 
 const DevSixer = ({ show, setShow, theme, setTheme }) => {
     return (
         <div>
-            <Home></Home>
             <Routes>
                 <Route index element={<Home show={show} setShow={setShow} them={theme} setTheme={setTheme} />} />
+                <Route path="/signIn" element={<SignIn></SignIn>} />
+                <Route path="/signUp" element={<SignUp></SignUp>} />
             </Routes>
         </div>
     );
