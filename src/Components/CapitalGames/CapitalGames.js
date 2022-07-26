ee8be29d80fcf46a1a5ca42b665f733eaa4a2a9e
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-07-26 14:53:23 +0600

    samthing code

---
+++ src/Components/CapitalGames/CapitalGames.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CapitalGames = () => {
+    return (
+        <div>
+            This is CapitalGames
+        </div>
+    );
+};
+
+export default CapitalGames;
--- src/Components/DevSixer.js
+++ src/Components/DevSixer.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import CapitalGames from './CapitalGames/CapitalGames';
 import Home from './Home/Home';
 import NotFound from './NotFound/NotFound';
 
@@ -7,6 +8,7 @@ const DevSixer = ({ show, setShow, theme, setTheme }) => {
         <div>
             <Routes>
                 <Route path='/' element={<Home show={show} setShow={setShow} them={theme} setTheme={setTheme} />} />
+                <Route path='/CapitalGames' element={<CapitalGames show={show} setShow={setShow} them={theme} setTheme={setTheme} />} />
                 <Route path='*' element={<NotFound show={show} setShow={setShow} them={theme} setTheme={setTheme} />} />
                 
             </Routes>
