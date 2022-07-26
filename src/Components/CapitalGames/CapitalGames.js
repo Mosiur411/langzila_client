cd691b9c09465d5e88a58b927c4962ef32c666ac
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-07-26 16:53:58 +0600

    audio

--- src/Components/CapitalGames/CapitalGames.js
+++ src/Components/CapitalGames/CapitalGames.js
@@ -28,17 +28,16 @@ const CapitalGames = () => {
     return (
         <div className='px-2 md:px-10 font-bold uppercase'>
             <h1 className="text-4xl text-center py-10">CapitalGames</h1>
-            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-10'>
+            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-24'>
                 {
                     CapitalLetter.map(word =>
                         <div
                             onMouseEnter={() => AudioPlay(word.Latter)}
-                            className='w-[150px] h-[150px] mx-auto text-2xl flex justify-center items-center bg-[#FE4A55]
+                            className='w-full h-[170px] text-7xl flex justify-center items-center bg-[#FE4A55]
                             hover:shadow-amber-500 hover:shadow-2xl text-white rounded'
                         >{word.Latter}</div>
                     )
                 }
-
             </div>
         </div>
     );
