2ba0c37bd42ed481b1a118bb4f646fe26fa4ed22
arfat132 <arfatbegum90@gmail.com>
2022-07-26 20:12:38 +0300

    route added

--- src/Components/DevSixer.js
+++ src/Components/DevSixer.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import {Routes,Route } from 'react-router-dom';
 import SignIn from './Auth/SignIn';
 import SignUp from './Auth/SignUp';
-=======
 import { Routes, Route } from 'react-router-dom';
 import Contact from './Contact/Contact';
->>>>>>> 594bfa1eca14f4931c0b53986532254fb52d3e57
 import Home from './Home/Home';
 
 const DevSixer = ({ show, setShow, theme, setTheme }) => {
@@ -13,12 +10,9 @@ const DevSixer = ({ show, setShow, theme, setTheme }) => {
         <div>
             <Routes>
                 <Route index element={<Home show={show} setShow={setShow} them={theme} setTheme={setTheme} />} />
-<<<<<<< HEAD
                 <Route path="/signIn" element={<SignIn></SignIn>} />
                 <Route path="/signUp" element={<SignUp></SignUp>} />
-=======
                 <Route index path="/contact" element={<Contact show={show} setShow={setShow} them={theme} setTheme={setTheme} />} />
->>>>>>> 594bfa1eca14f4931c0b53986532254fb52d3e57
             </Routes>
         </div>
     );
