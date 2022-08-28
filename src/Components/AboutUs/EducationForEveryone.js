c086513d91ee48f3fd4cec2a0d48093b94154c6b
Mosiur <mosiurislamwebdesign@gmail.com>
2022-08-28 15:12:00 +0600

    aboutbar clear

--- src/Components/AboutUs/EducationForEveryone.js
+++ src/Components/AboutUs/EducationForEveryone.js
@@ -5,7 +5,7 @@ import { MdPublishedWithChanges } from 'react-icons/md';
 
 const EducationForEveryone = () => {
     return (
-        <section class="lg:px-20 mt-96 lg:mt-0">
+        <section class=" lg:px-20 mt-96 lg:mt-0">
             <div class="px-5 py-24 mx-auto">
                 <div class="text-center mx-auto mb-[60px] max-w-[610px]">
                     <span class="font-semibold text-md uppercase text-primary mb-3 block">
@@ -16,30 +16,30 @@ const EducationForEveryone = () => {
                     </h2>
                     <p>Finding your own space and utilize better learning options can result in faster than the traditional ways. Enjoy the beauty of eLearning!</p>
                 </div>
-                <div class="flex flex-wrap -m-4 text-center">
+                <div class="flex flex-wrap -m-4 text-center overflow-hidden">
                     <div class="p-4 lg:w-1/4 md:w-1/2 w-full h-80">
-                        <div class="shadow-lg border-l-4 border-l-primary px-5 py-6 rounded-lg h-[270px]">
+                        <div class="shadow-lg border-l-4 border-l-primary px-5 py-6 rounded-lg h-[270px] overflow-hidden">
                             <GiBrain className='text-6xl mx-auto text-primary mb-4' />
                             <h2 class="title-font font-bold text-xl mb-2 text-accent">Learn the Latest Top Skills</h2>
                             <p class="leading-relaxed">Learning top skills can bring an extra-ordinary outcome in a career.</p>
                         </div>
                     </div>
-                    <div class="p-4 lg:w-1/4 md:w-1/2 sm:w-1/2 w-full">
-                        <div class="shadow-lg border-l-4 border-l-primary px-5 py-6 rounded-lg h-[270px]">
+                    <div class="p-4 lg:w-1/4 md:w-1/2 sm:w-1/2 w-full ">
+                        <div class="shadow-lg border-l-4 border-l-primary px-5 py-6 rounded-lg h-[270px] overflow-hidden">
                             <SiGoogleclassroom className='text-6xl mx-auto text-primary mb-4' />
                             <h2 class="title-font font-bold text-xl mb-2 text-accent">Learn in Your Own Pace</h2>
                             <p class="leading-relaxed">Everyone prefers to enjoy learning at their own pace & that gives a great result.</p>
                         </div>
                     </div>
                     <div class="p-4 lg:w-1/4 md:w-1/2 sm:w-1/2 w-full">
-                        <div class="shadow-lg border-l-4 border-l-primary px-5 py-6 rounded-lg h-[270px]">
+                        <div class="shadow-lg border-l-4 border-l-primary px-5 py-6 rounded-lg h-[270px] overflow-hidden">
                             <MdPublishedWithChanges className='text-6xl mx-auto text-primary mb-4' />
                             <h2 class="title-font font-bold text-xl mb-2 text-accent">Learn From Industry Experts</h2>
                             <p class="leading-relaxed">Experienced teachers can assist in learning faster with their best approaches!</p>
                         </div>
                     </div>
                     <div class="p-4 lg:w-1/4 md:w-1/2 sm:w-1/2 w-full">
-                        <div class="shadow-lg border-l-4 border-l-primary rounded-lg px-5 py-6 h-[270px]">
+                        <div class="shadow-lg border-l-4 border-l-primary rounded-lg px-5 py-6 h-[270px] overflow-hidden">
                             <GiWorld className='text-6xl mx-auto text-primary mb-4' />
                             <h2 class="title-font font-bold text-xl mb-2 text-accent">Enjoy Learning From Anywhere</h2>
                             <p class="leading-relaxed">We are delighted to give you options to enjoy learning from anywhere in the world.</p>
--- src/Components/AboutUs/ProjectsVideo.js
+++ src/Components/AboutUs/ProjectsVideo.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 
 const ProjectsVideo = () => {
+    const [widthhynakan, setwidthhynaka] = useState(false)
+
+    window.addEventListener('resize', () => {
+        if (document.documentElement.clientWidth >= 900) {
+            setwidthhynaka(!widthhynakan)
+        }
+    })
+
     return (
-        <div className='lg:ml-96 md:ml-8 px-5 relative'>
-            <div>
-                <ReactPlayer className="absolute lg:hidden block" width={'400px'} controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-                <ReactPlayer className="absolute lg:hidden md:block hidden" width={'700px'} height={'400px'} controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-                <ReactPlayer className="absolute lg:block hidden" width={'800px'} height={'500px'} controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+        <div className='relative'>
+            <div className='w-full mx-auto'>
+                <ReactPlayer width={widthhynakan ? "300px" : "700px"} className="absolute top-0 left-0 right-0 bottom-0  mx-auto" controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
                 <div>
-                    <img className='relative -ml-24 pt-24 lg:block hidden' src="https://themes.envytheme.com/ecademy/wp-content/uploads/2020/05/shape9.png" alt="" />
+                    <img className='relative ml-60 pt-24 lg:block hidden' src="https://themes.envytheme.com/ecademy/wp-content/uploads/2020/05/shape9.png" alt="" />
                 </div>
             </div>
         </div>
